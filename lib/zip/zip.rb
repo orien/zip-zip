0d98b021b3fa85085d251b3dbdd1cad0e4de2ca2
Orien Madgwick <orien.madgwick@envato.com>
2013-09-20 22:19:31 +1000

    Alias classes to their pre v1.0.0 names

--- lib/zip/zip.rb
+++ lib/zip/zip.rb
@@ -1,5 +1,13 @@
 # encoding: utf-8
 require 'zip'
 
-Zip::ZipFile = Zip::File
-Zip::ZipEntry = Zip::Entry
+Zip::ZipCentralDirectory     =  Zip::CentralDirectory
+Zip::ZipEntry                =  Zip::Entry
+Zip::ZipEntrySet             =  Zip::EntrySet
+Zip::ZipExtraField           =  Zip::ExtraField
+Zip::ZipFile                 =  Zip::File
+Zip::ZipFileSystem           =  Zip::FileSystem
+Zip::ZipInputStream          =  Zip::InputStream
+Zip::ZipOutputStream         =  Zip::OutputStream
+Zip::ZipStreamableDirectory  =  Zip::StreamableDirectory
+Zip::ZipStreamableStream     =  Zip::StreamableStream
